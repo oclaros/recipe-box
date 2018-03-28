1feaeea5e4630c76e58c009bea45e7a23455f951
Oscar Claros <oclaros1@hotmail.com>
2018-03-27 20:01:04 -0500

    changed index_bundle.js to bundle.js in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     entry: './src/index.js', 
     output: { 
       path: path.resolve('dist'), 
-      filename: 'index_bundle.js' 
+      filename: 'bundle.js' 
     }, 
     module: { 
       rules: [ 
